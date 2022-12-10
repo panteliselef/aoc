d24db00e2b4d1eec32e428a5893335c218d3c4ed
Elefcodes <panteliselef@outlook.com>
2022-12-10 22:33:16 +0100

    day9 part 2
    solved day9 p2
    
    day9 part 2

--- src/day9.ts
+++ src/day9.ts
@@ -1,8 +1,16 @@
 import { readFileSync } from 'fs'
 
-const directions = readFileSync('inputs/day9.txt', 'utf-8').split('\n').map(row => row.split(' '))
+
+type Point = { x: number, y: number }
+type PointArray = [number, number]
 
 
+let movement: { [key: string]: Point } = {
+    'U': { x: 0, y: -1 },
+    'D': { x: 0, y: 1 },
+    'L': { x: -1, y: 0 },
+    'R': { x: 1, y: 0 },
+};
 const l = 1000
 
 const grid = new Array(l).fill('.').map(() => new Array(l).fill('.'))
@@ -12,80 +20,99 @@ const printGrid = (grid: string[][]) => {
     console.log('----------------')
     grid.forEach(row => {
         console.log(row.join(''))
-        console.log('\n')
     })
 }
 
 
-let tPos = [Math.floor(movementGrid.length / 2), Math.floor(movementGrid[0].length / 2)]
-let hPos = [Math.floor(movementGrid.length / 2), Math.floor(movementGrid[0].length / 2)]
-grid[tPos[0]][tPos[1]] = '#'
-movementGrid[hPos[0]][hPos[1]] = 'H'
+const directions = readFileSync('inputs/day9part2.txt', 'utf-8').split('\n').map(row => row.split(' '))
 
 
-let movement: { [key: string]: { x: number, y: number } } = {
-    'U': { x: 0, y: -1 },
-    'D': { x: 0, y: 1 },
-    'L': { x: -1, y: 0 },
-    'R': { x: 1, y: 0 },
-};
+const middleRow = Math.floor(grid.length / 2);
+const middleCol = Math.floor(grid[0].length / 2);
+
+const positions: PointArray[] = [];
+
+const length = 10
+
+for (let i = 0; i < length; i++) {
+    positions.push([middleRow, middleCol])
+}
+
+positions.forEach(([row, col]) => {
+    grid[row][col] = '#'
+})
 
 
-const moveT = () => {
 
-    const [hRow, hCol] = hPos;
-    const [tRow, tCol] = tPos;
+const calcTailPositionFromHead = (head: PointArray, tail: PointArray) => {
 
-    const rowDiff = (hRow - tRow);
-    const colDiff = (hCol - tCol);
+    const [hRow, hCol] = head;
+    const [tRow, tCol] = tail;
 
-    let newTPost = [tRow, tCol];
+    let newTPost = [tRow, tCol]satisfies PointArray;
 
-    if (rowDiff > 1 && colDiff === 0) {
-        newTPost = [hRow - 1, newTPost[1]];
+
+    const dx = Math.abs((hCol - tCol))
+    const dy = Math.abs((hRow - tRow))
+
+    if ((dx === 0 && dy === 0) || dx + dy === 1 || (dx === 1 && dy === 1)) {
+        return newTPost
     }
 
-    if (colDiff > 1 && rowDiff === 0) {
-        newTPost = [newTPost[0], hCol - 1];
+    /*T.H.. -> .TH..*/
+    if (dx === 0) {
+        newTPost = [(hRow + tRow) / 2, newTPost[1]];
     }
-
-    if (rowDiff > 1 && colDiff !== 0) {
-        newTPost = [hRow - 1, hCol];
+    /*..H..    ..H..
+      ..... -> ..T..
+      ..T..    ..... */
+    if (dy === 0) {
+        newTPost = [newTPost[0], (hCol + tCol) / 2];
     }
-
-    if (colDiff > 1 && rowDiff !== 0) {
-        newTPost = [hRow, hCol - 1];
+    /*..H..    ..H..
+      ..... -> .T...
+      T....    .....*/
+    if (dx > 1 && dy > 1) {
+        newTPost = [(hRow + tRow) / 2, (hCol + tCol) / 2];
     }
-
-
-    if (rowDiff < -1 && colDiff !== 0) {
-        newTPost = [hRow + 1, hCol];
+    /*..H..    .TH..
+      T.... -> .....
+      .....    .....*/
+    if (dx > dy) {
+        newTPost = [hRow, (hCol + tCol) / 2];
     }
-
-    if (colDiff < -1 && rowDiff !== 0) {
-        newTPost = [hRow, hCol + 1];
-    }
-
-    if (rowDiff < -1 && colDiff === 0) {
-        newTPost = [hRow + 1, newTPost[1]];
-    }
-
-    if (colDiff < -1 && rowDiff === 0) {
-        newTPost = [newTPost[0], hCol + 1];
+    /*..H..    ..H..
+      ..... -> ..T..
+      .T...    .....*/
+    if (dy > dx) {
+        newTPost = [(hRow + tRow) / 2, hCol];
     }
 
     return newTPost
 }
 
 
+movementGrid[positions[0][0]][positions[0][1]] = `${0}`
+
 directions.forEach(([dir, steps]) => {
     const { x, y } = movement[dir]
     for (let i = 0; i < +steps; i++) {
-        hPos = [hPos[0] + y, hPos[1] + x]
-        movementGrid[hPos[0]][hPos[1]] = 'H'
-        tPos = moveT();
-        grid[tPos[0]][tPos[1]] = '#'
-        movementGrid[tPos[0]][tPos[1]] = 'T'
+
+        positions[0] = [positions[0][0] + y, positions[0][1] + x]
+
+        for (let j = 0; j < length - 1; j++) {
+
+            positions[j + 1] = calcTailPositionFromHead(positions[j], positions[j + 1])
+
+            movementGrid[positions[j][0]][positions[j][1]] = `${j}`
+            movementGrid[positions[j + 1][0]][positions[j + 1][1]] = `${j + 1}`
+
+            if (j + 1 === length - 1) {
+                grid[positions[j + 1][0]][positions[j + 1][1]] = '#'
+            }
+
+
+        }
     }
 })
 
